d17dc4b385ea8d0392b248b6566663a79945e37e
ieshualet <mlieshua08@gmail.com>
2025-09-05 10:15:31 -0300

    commit
    éisso aí

--- src/screens/signln/index.tsx
+++ src/screens/signln/index.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import { styles
-    
- } from './styles';
+import { styles } from './styles';
+import {TextInput }  from 'react-native'
+import { useState } from 'react';
+
 export function Signin() {
+
+const [text, setText] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Componente de teste</Text>
+      <Text>primeiro projeito da letícia-orgulhosa</Text>
+      <TextInput style={{width:200, height:50, borderWidth:3}} onChangeText={setText}/>
+        <Text> Você digitou: {text} </Text>
+        <Text> Que legal!</Text>
       <StatusBar style="auto" />
     </View>
   );
