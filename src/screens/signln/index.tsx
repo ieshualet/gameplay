c343582710e9cf8d582b430e3b9abf31bbfabd65
ieshualet <mlieshua08@gmail.com>
2025-10-03 08:26:47 -0300

    commit
    .

--- src/screens/signln/index.tsx
+++ src/screens/signln/index.tsx
@@ -8,6 +8,17 @@ export function Signin() {
   return (
        <View style={stylesSignIn.container}>
         <Image source={illustration} style={stylesSignIn.image} resizeMode="stretch"/>
+        <View style={stylesSignIn.content}>
+          <Text style={stylesSignIn.tittle}>
+            Organize suas{`\n`}
+            jogatinas{`\n`}
+            facilmente
+          </Text>
+          <Text style={stylesSignIn.subtittle}>
+            Crie grupos pra jogar seus games{`\n`}
+            favoritos com seus amigos
+          </Text>
+        </View>
        </View>
   );
 }
--- src/screens/signln/styles.ts
+++ src/screens/signln/styles.ts
@@ -1,14 +1,24 @@
 import{ StyleSheet } from "react-native"; 
+import { theme } from "../../global/styles/theme";
 
 export const stylesSignIn = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffffff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor:theme.colors.background
   },
   image:{
     width:'100%',
     height:360
+  },
+  content:{
+    marginTop:-40
+  },
+  tittle:{
+    color:theme.colors.header,
+    fontSize:40,
+    marginBottom:16,
+    textAlign:'center'
   }
 });
